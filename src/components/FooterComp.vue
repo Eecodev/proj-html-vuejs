91c2484c32e8b3a150b31ba79ec19aa5c169ec0d
Eecodev <mnasser.bbk@gmail.com>
2023-11-22 12:08:25 +0100

    mid-foot section dynamically generated with v-for

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -3,12 +3,10 @@
     <footer>
         <!-- subscribe section -->
         <div id="top-foot" class="container">
-            <div>
-                <div class="input-group mb-3 d-flex">
-                    <input type="text" class="form-control w-25 " placeholder="Subscribe to our newsletter">
-                    <div class="input-group-append">
-                        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Register</button>
-                    </div>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control my-custom-width" placeholder="Subscribe to our newsletter">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary my-radius" type="button" id="button-addon2">Register</button>
                 </div>
             </div>
             <div>
@@ -19,9 +17,9 @@
 
         <!-- contact us section -->
         <div id="mid-foot" class="container">
-            <h3>{{ title }}</h3>
-            <ul>
-                <li v-for="contact in contactUs" :key="contact.id">{{ contact }}</li>
+            <ul v-for="list in footerExplore" :key="list.id"> 
+                <h3>{{ list.title }} </h3>
+                <li v-for="item in list.items" :key="item.id">{{ item.content }}</li>
             </ul>
         </div>
 
@@ -38,21 +36,93 @@
         name: 'FooterComp',
         data() {
             return {
-                contactUs:
-                    {
+                footerExplore:[
+                    {   
+                        id: 1,
                         title: 'Contact Us',
-                        address: '900 Wood Street, MI New York 48607',
-                        phoneNum: '+56 345 678 000-09',
-                        email: 'info@example.com'
+                        items: [
+                            {
+                                content: '900 Wood Street, MI New York 48607'
+                            },
+                            {
+                                content: '+56 345 678 000-09'
+                            },
+                            {
+                                content: 'info@example.com'
+                            }
+                        ]
                     },
-                    explore: [
-                        
-                    ]
-                }
+                    {   
+                        id: 2,
+                        title: 'Support Us',
+                        items: [
+                            {
+                                content: 'Privacy'
+                            },
+                            {
+                                content: 'Terms'
+                            },
+                            {
+                                content: 'Cookie Policy'
+                            },
+                            {
+                                content: 'Sitemap'
+                            }
+                        ]
+                    },
+                    {
+                        id: 3,
+                        title: 'Explore',
+                        items: [
+                            {
+                                content: 'Accomodation'
+                            },
+                            {
+                                content: 'Arts Academics'
+                            },
+                            {
+                                content: 'Central Services'
+                            },
+                            {
+                                content: 'Conferences and Events'
+                            },
+                            {
+                                content: 'Food and Drink'
+                            }
+                        ]
+                    },
+                    {
+                        id: 4,
+                        title: 'Visit',
+                        items: [
+                            {
+                                content: 'Admission'
+                            },
+                            {
+                                content: 'Contact Us'
+                            },
+                            {
+                                content: 'FAQs'
+                            },
+                            {
+                                content: 'Request Information'
+                            },
+                            {
+                                content: 'Privacy Notice'
+                            }
+                        ]
+                    }
+                ]
             }
         }
+     }
 </script>
 
 <style lang="scss" scoped>
-
+    .my-custom-width{
+        max-width: 25%;
+    }
+    .my-radius{
+        border-radius: 0;
+    }
 </style>
